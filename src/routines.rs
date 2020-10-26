a520b969a89f3cceb10222e8c01ae4799638542c
Nikita.m <nikita.m@tonlabs.io>
2020-10-26 17:28:39 +0300

    Check that hash is a number

--- src/routines.rs
+++ src/routines.rs
@@ -2,6 +2,8 @@ use chrono::{TimeZone, Local};
 use ton_client_rs::{TonClient, Ed25519KeyPair};
 use ed25519_dalek::{Keypair, Signature};
 use ed25519::signature::Signer;
+use num_bigint::BigUint;
+use num_traits::Num;
 
 pub fn call_routine(
     ton: &TonClient,
@@ -108,10 +110,15 @@ pub(super) fn load_boc_from_file(_ton: &TonClient, arg: &str) -> Result<String, 
 fn extract_hash(arg: &str) -> Result<Vec<u8>, String> {
     let arg_json: serde_json::Value = serde_json::from_str(arg)
         .map_err(|e| format!("argument is invalid json: {}", e))?;
-    let hash_str = arg_json["hash"].as_str()
+    let mut hash_str = arg_json["hash"].as_str()
         .ok_or(format!(r#""hash" argument not found"#))?;
-    let hash_str = hash_str.get(2..).ok_or("hash is not an uint256 number".to_owned())?;
-    hex::decode(&format!("{:0>64}", hash_str))
+    if hash_str.starts_with("0x") {
+        hash_str = hash_str.get(2..)
+            .ok_or("hash is not an uint256 number".to_owned())?;
+    }
+    let hash_int = BigUint::from_str_radix(hash_str, 16)
+        .map_err(|_| "hash is not an uint256 number".to_owned())?;
+    hex::decode(&format!("{:0>64}", hash_int.to_str_radix(16)))
         .map_err(|e| {
             format!("failed to decode hash from hex string:\n hash: {}\n error: {}", hash_str, e)
         })
@@ -166,5 +173,20 @@ mod tests {
         let valid_hash = hex::decode("002461b752243bba76ad56fe14f88d2d0bb224c68f1c598dd3a34ee3204ddc80").unwrap();
         let arg = json!({ "hash": hash3 }).to_string();
         assert_eq!(valid_hash, extract_hash(&arg).unwrap());
+    }
+
+    #[test]
+    fn test_extract_hash_3() {
+        let hash = "32461b752243bba76ad56fe14f88d2d0bb224c68f1c598dd3a34ee3204ddc84";
+        let arg = json!({ "hash": hash }).to_string();
+        let valid_hash = hex::decode("032461b752243bba76ad56fe14f88d2d0bb224c68f1c598dd3a34ee3204ddc84").unwrap();
+        assert_eq!(valid_hash, extract_hash(&arg).unwrap());
+    }
+
+    #[test]
+    fn test_extract_hash_4() {
+        let hash = "qwerty";
+        let arg = json!({ "hash": hash }).to_string();
+        assert_eq!(true, extract_hash(&arg).is_err());
     }
 }
